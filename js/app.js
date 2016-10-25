9c1e89b2a0503fef7d68345b12a9843ee4597644
YRadov-ПК\YRadov <radov.yuriy@ukr.net>
2016-10-25 22:00:29 +0300

    first React Component

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
+var App = React.createClass({
+    render: function() {
+        return (
+           <div className="app">
+               Компонент Арр!
+           </div>
+        );
+    }
+});
+
 ReactDOM.render(
-    <h1>Hello,	world!</h1>,
-    document.getElementById('root')
-)
+    <App />,
+document.getElementById('root')
+);
