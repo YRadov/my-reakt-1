0ded34bb01b742946b5ada21c89304e1ab629bab
YRadov-ПК\YRadov <radov.yuriy@ukr.net>
2016-10-26 22:35:19 +0300

    use setState

--- js/app.js
+++ js/app.js
@@ -1,27 +1,57 @@
 var	my_news	=	[
     {
         author:	'Саша Печкин',
-        text:	'В четверг, четвертого числа...'
+        text:	'В четверг, четвертого числа...',
+        bigText: 'Какойто большой текст. Какойто большой текст.Какойто большой текст. Какойто большой текст.Какойто большой текст. Какойто большой текст.Какойто большой текст. Какойто большой текст.'
     },
     {
         author:	'Просто	Вася',
-        text:	'Считаю, что $ должен стоить 35 рублей!'
+        text:	'Считаю, что $ должен стоить 35 рублей!',
+        bigText: 'Какойто большой текст. Какойто большой текст.Какойто большой текст. Какойто большой текст.Какойто большой текст. Какойто большой текст.Какойто большой текст. Какойто большой текст.'
     },
     {
         author:	'Гость',
-        text:	'Бесплатно.	Скачать. Лучший	сайт - http://localhost:3000'
+        text:	'Бесплатно.	Скачать. Лучший	сайт - http://localhost:3000',
+        bigText: 'Какойто большой текст. Какойто большой текст.Какойто большой текст. Какойто большой текст.Какойто большой текст. Какойто большой текст.Какойто большой текст. Какойто большой текст.'
     }
 ];
 
 var Article = React.createClass({
-    render:	function()	{
+        propTypes: {
+            data: React.PropTypes.shape({
+                author: React.PropTypes.string.isRequired,
+                text: React.PropTypes.string.isRequired,
+                bigText: React.PropTypes.string.isRequired
+            })
+        },
+        getInitialState: function() {
+            return {
+                visible: false
+            };
+        },
+        readMoreClick: function(e) {
+            e.preventDefault();
+            this.setState({visible: true}, function() {
+                alert('Состояние изменилось');
+            });
+        },
+        render:	function()	{
         var author = this.props.data.author,
-            text = this.props.data.text;
+            text = this.props.data.text,
+            bigText = this.props.data.bigText,
+            visible = this.state.visible;
 
         return	(
             <div className="article">
                 <p className="news_author">{author}</p>
                 <p className="news_text">{text}</p>
+                <a href="#" onClick={this.readMoreClick}
+                   className={'news_readmore ' + (visible ? 'none' : '')}>
+                    Подробнее
+                </a>
+                <p className={'news_big-text '  + (visible ? '' : 'none')}>
+                    {bigText}
+                </p>
             </div>
 
         );
@@ -29,6 +59,9 @@ var Article = React.createClass({
 });
 
 var News = React.createClass({
+    propTypes: {
+        data: React.PropTypes.array.isRequired
+    },
     render:	function()	{
         var data = this.props.data;
         var newsTemplate;
